fbb37ba180a40809378e62e2df830bfbeeb7bac1
m-browntown <sdmattbrown@gmail.com>
2021-03-03 21:14:05 -0800

    New branch

--- src/main/java/xyz/veracode/verainsecure/XSS.java
+++ src/main/java/xyz/veracode/verainsecure/XSS.java
@@ -39,10 +39,8 @@ public class XSS extends HttpServlet {
 		//Reflective XSS
 		
 		String username = req.getParameter("xssusername");
-		String vulnparam = req.getParameter("xssvulnparam");
 		String htmlResponse = "<html>";
 		htmlResponse += "<h2>Your xss vulnerable username is: " + username + "<br/>";
-		//htmlResponse += "<h2>Your other vulnerable param is: " + vulnparam + "<br/>";
 		htmlResponse += "</html>";
 		writer.println(htmlResponse);
 		
